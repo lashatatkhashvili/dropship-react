1289b41d5f10e26af1d456f8a9ee77ca2603e17d
ltatkhashvili <d.karseladze@moodia.net>
2021-05-14 12:06:11 +0400

    help

--- src/helpData.js
+++ src/helpData.js
@@ -13,7 +13,52 @@ const helpData = [
     name: "Dashboard",
     heading: "Dashboard",
     paragraph:
-      "“Dashboard” contains all necessary personal details including first name, last name, phone, country, email, Skype and photo. You can change your password, deactivate your account and sign out. In “My Profile-Billing” you will be able to upgrade subscription plan of your account.",
+      "In Dashboard you will find information about our Niches, Quantity of Products, Inventory, Orders, Deposit Status, Current Plan, Help Section, Location of our Suppliers and NEWS.",
+    video: "dashboard",
+  },
+
+  {
+    id: 2,
+    name: "Catalog-Searching products",
+    heading: "Searching products in the catalog",
+    paragraph:
+      "Enter any name of a product into searching field on the right side, press “search” and view the results",
+    video: "dashboard",
+  },
+
+  {
+    id: 3,
+    name: "Catalog-Sorting Products",
+    heading: "Sorting Products in the Catalog",
+    paragraph:
+      "In the Catalog click the button “Sort by: New Arrivals” in order to sort products by “Profit” or “Price” from Low to High or High to Low",
+    video: "dashboard",
+  },
+
+  {
+    id: 4,
+    name: "Catalog-Advanced Filters",
+    heading: "Advanced Filters",
+    paragraph:
+      "In the Catalog click the button “Sort by: New Arrivals” in order to sort products by “Profit” or “Price” from Low to High or High to Low",
+    video: "dashboard",
+  },
+
+  {
+    id: 5,
+    name: "Catalog-Selecting Filters",
+    heading: "Selecting products in the catalog",
+    paragraph:
+      "In the Catalog click the button “Sort by: New Arrivals” in order to sort products by “Profit” or “Price” from Low to High or High to Low",
+    video: "dashboard",
+  },
+
+  {
+    id: 6,
+    name: "Catalog-Buying products",
+    heading: "Buying products",
+    paragraph:
+      "In the Catalog click the button “Sort by: New Arrivals” in order to sort products by “Profit” or “Price” from Low to High or High to Low",
     video: "dashboard",
   },
 ];
